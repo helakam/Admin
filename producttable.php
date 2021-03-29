7e1d0e83b87ad37d882ad97832b7fcdacfeefa64
Pasindu Sooriyabandara <56555775+PasinduChanusha@users.noreply.github.com>
2021-03-29 13:17:47 +0530

    Add files via upload

---
+++ producttable.php
@@ -0,0 +1,50 @@
+
+<h2 align="center">PRODUCTS</h2>
+ <link rel="stylesheet" href="posttable1.css">
+<div class="table-wrapper">
+    <table class="fl-table">
+        <thead>
+        <tr>
+          <!--  <th>ID</th> -->
+            <th>NAME</th>
+            <th>DESCRIPTION</th>
+            <th>COLOR</th>
+            <th>PRICE</th>
+            <th>ADD</th>
+            <th>REJECT</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr>
+           <?php session_start(); ?>
+<?php require_once("ImConnect.php") ?>
+<?php 
+ $sql ="SELECT p_id,p_name,p_cat,p_des,p_color,p_price,p_quantity,p_weight,p_path1 FROM product";
+ $result =mysqli_query($connection,$sql);
+ if($result-> num_rows != 0){
+ while ($row = $result-> fetch_assoc()){
+    //echo"<tr><td>".$row["p_id"]."</td>";
+    echo"<td>".$row["p_name"]."</td>";
+    echo"<td>".$row["p_des"]."</td>";
+    echo"<td>".$row["p_color"]."</td>";
+    echo"<td>".$row["p_price"]."</td>";
+   $_SESSION["img"] = $row["p_path1"];
+    echo"<td>"."<a href=accept.php?id=".$row["p_id"].">Add</a></td>";
+    echo"<td>"."<a href=deleteP.php?id=".$row["p_id"].">Reject</a>"."</td></tr>";
+
+ } 
+ echo "</table>";
+}
+else{
+    echo "0 result";
+
+}
+$connection-> close();
+ ?>     
+        </tr>
+        
+        <tbody>
+    </table>
+</div>
+
+
